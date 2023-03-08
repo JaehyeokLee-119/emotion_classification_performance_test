95d06b49a61088dc08b713f612fbd13546b772f3
Lee Jae Hyeok <hjl8708@dubai.hlilab.kr>
2023-03-08 15:56:57 +0900

    :sparkles: main.py와 tester.py 추가

---
+++ main.py
@@ -0,0 +1,68 @@
+import argparse
+import json
+import os
+import random
+import csv
+from typing import List
+
+from module.tester import TestEnv
+
+import numpy as np
+import torch
+
+# Reproducibility setting
+def set_random_seed(seed: int):
+    random_seed = seed
+    torch.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    # Training Environment
+    parser.add_argument('--gpus', default=[1])
+    parser.add_argument('--num_process', default=int(os.cpu_count() * 0.8), type=int)
+    parser.add_argument('--num_worker', default=6, type=int)
+    parser.add_argument('--port', default=1234, type=int)
+
+    parser.add_argument('--model_name', default="j-hartmann/emotion-english-distilroberta-base")
+    parser.add_argument('--different_topology', default=False)  # pretrained emotion classification model to test has different emotion topology as DailyDialog
+    parser.add_argument('--topology', default=None)             # The number of emotion classes in the model (if the model has different topology)
+    parser.add_argument('--epoch', default=10, type=int)        # When fine-tuning is required
+    parser.add_argument('--dropout', default=0.5, type=float)   # When fine-tuning is required
+    
+    parser.add_argument('--test_data', default="data/data_fold/data_0/dailydialog_test.json")
+    parser.add_argument('--log_directory', default='logs', type=str)
+    parser.add_argument('--data_label', help='the label that attaches to saved model', default='dailydialog_fold_0')
+
+    parser.add_argument('--max_seq_len', help='the max length of each tokenized utterance', default=75, type=int)
+    return parser.parse_args()
+
+def test_preconditions(args: argparse.Namespace):
+    assert args.model_name is not None, "For test, load emotion classification model."
+
+def main():
+    args = parse_args()
+
+    test_preconditions(args)
+    set_random_seed(77)
+    
+    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join([str(_) for _ in args.gpus])
+    
+    test_data_list = ['data_fold/data_0/dailydialog_test.json']
+    
+    for te in test_data_list:
+        args.test_data = te
+        
+        tester = TestEnv(**vars(args))
+        tester.run(**vars(args))
+        
+        del tester
+
+if __name__ == '__main__':
+    main()
---
+++ module/tester.py
@@ -0,0 +1,3 @@
+
+class TestEnv:
+    pass
