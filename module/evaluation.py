e62cb69c61ec39e69a8350647de9ebcf908a8eac
Lee Jae Hyeok <hjl8708@g.skku.edu>
2023-03-13 12:43:16 +0900

    :sparkles: evaluation.py에 FocalLoss 추가

--- module/evaluation.py
+++ module/evaluation.py
@@ -13,12 +13,20 @@ emotion_label_policy = {'angry': 0, 'anger': 0,
     'neutral': 6}
 
 def argmax_prediction(pred_y, true_y):
+    '''
+    역할: 각 클래스 별 점수가 담긴 텐서를 argmax를 통해 정답 레이블과 같은 형태로 변환
+    pred_y: (batch_size, num_class) : 각 클래스 별 점수가 담긴 텐서
+    true_y: (batch_size, )          : 정답 레이블이 담긴 텐서 (정수)
+    '''
     pred_argmax = torch.argmax(pred_y, dim=1).cpu()
     true_y = true_y.cpu()
     return pred_argmax, true_y
 
 
 def threshold_prediction(pred_y, true_y):
+    '''
+    역할: Binary Classification 결과를 점수 0.5를 1차원 값(0/1)으로 변환 
+    '''
     pred_y = pred_y > 0.5
     return pred_y, true_y
 
@@ -38,7 +46,10 @@ def metrics_report(pred_y, true_y, label, get_dict=False, multilabel=False):
         return classification_report(true_y, pred_y, target_names=class_name, zero_division=0, digits=4)
 
 
-def metrics_report_for_emo_binary(pred_y, true_y, neutral_num, get_dict=False, multilabel=False):
+def metrics_report_for_emo_binary(pred_y, true_y, get_dict=False, multilabel=False):
+    '''
+    주석: 
+    '''
     if multilabel:
         pred_y, true_y = threshold_prediction(pred_y, true_y)
         available_label = sorted(list(set((pred_y == True).nonzero()[:, -1].tolist() + (true_y == True).nonzero()[:, -1].tolist())))
@@ -47,8 +58,8 @@ def metrics_report_for_emo_binary(pred_y, true_y, neutral_num, get_dict=False, m
         available_label = sorted(list(set(true_y.tolist() + pred_y.tolist())))
 
     class_name = ['non-neutral', 'neutral']
-    pred_y = [1 if element == neutral_num else 0 for element in pred_y] # element 6 means neutral.
-    true_y = [1 if element == neutral_num else 0 for element in true_y]
+    pred_y = [1 if element == 6 else 0 for element in pred_y] # element 6 means neutral.
+    true_y = [1 if element == 6 else 0 for element in true_y]
 
     if get_dict:
         return classification_report(true_y, pred_y, target_names=class_name, zero_division=0, digits=4, output_dict=True)
@@ -67,3 +78,38 @@ def log_metrics(logger, emo_pred_y_list, emo_true_y_list, option='train'):
     report_dict = metrics_report_for_emo_binary(torch.cat(emo_pred_y_list), torch.cat(emo_true_y_list), get_dict=True)
     acc_emo, p_emo, r_emo, f1_emo = report_dict['accuracy'], report_dict['weighted avg']['precision'], report_dict['weighted avg']['recall'], report_dict['weighted avg']['f1-score']
     logger.info(f'\nemotion (binary): {option}\n')
+    
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1 - alpha])
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            input = input.view(input.size(0), input.size(1), -1)
+            input = input.transpose(1, 2)
+            input = input.contiguous().view(-1, input.size(2))
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = logpt.exp()
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * at
+
+        loss = -1 * (1 - pt)**self.gamma * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
