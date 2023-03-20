2981e2acf7c2068747d2603d2c251336c7d2b9b9
Lee Jae Hyeok <hjl8708@g.skku.edu>
2023-03-20 11:33:45 +0900

    :spakles: Transformer layer 1개 추가해서 finetuning 하는옵션 추가

--- main.py
+++ main.py
@@ -33,13 +33,15 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--model_name', default="j-hartmann/emotion-english-distilroberta-base")
     
     # Training Environment
-    parser.add_argument('--epoch', default=30, type=int)            # When fine-tuning is required
-    parser.add_argument('--dropout', default=0.2, type=float)       # When fine-tuning is required
-    parser.add_argument('--learning_rate', default=5e-5)            # When fine-tuning is required
+    parser.add_argument('--epoch', default=50, type=int)            # When fine-tuning is required
+    parser.add_argument('--dropout', default=0.5, type=float)       # When fine-tuning is required
+    parser.add_argument('--learning_rate', default=5e-4)            # When fine-tuning is required
     parser.add_argument('--batch_size', default=10, type=int)        # When fine-tuning is required
     
-    
     parser.add_argument('--mode', default='fine-tuning') # fine-tuning or test
+    parser.add_argument('--model_type', default=3) # fine-tuning additional model의 형태를 결정
+    parser.add_argument('--fine_tuned_model', default=None)
+    
     parser.add_argument('--taxonomy', default=False)  # pretrained emotion classification model to test has different emotion taxonomy as DailyDialog
     parser.add_argument('--n_emotion', default=7)  # pretrained emotion classification model to test has different emotion taxonomy as DailyDialog
     
@@ -49,8 +51,8 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--model_label', default='j-hartmann_emotion-english-distilroberta-base')
     
     # logging 관련
-    parser.add_argument('--log_directory', default='log_test', type=str)
-    parser.add_argument('--use_wandb', default=False)
+    parser.add_argument('--log_directory', default='log_model-type3-1layers_weights(5e-4)', type=str)
+    parser.add_argument('--use_wandb', default=True)
 
     parser.add_argument('--max_seq_len', help='the max length of each tokenized utterance', default=75, type=int)
     return parser.parse_args()
@@ -68,36 +70,32 @@ def main():
     
     # model_name = ["j-hartmann/emotion-english-distilroberta-base", "j-hartmann/emotion-english-roberta-large"]
     # model_label = ['j-hartmann distill roberta base', 'j-hartmann roberta large']
-    # model_name = ["j-hartmann/emotion-english-roberta-large"]
-    # model_label = ['j-hartmann_roberta_large']
+    model_name = ["j-hartmann/emotion-english-roberta-large"]
+    model_label = ['j-hartmann_roberta_large']
     # model_name = ["j-hartmann/emotion-english-distilroberta-base"]
     # model_label = ['j-hartmann_distill_roberta_base']
     
-    # model_name = ["michellejieli/emotion_text_classifier", 
-    #               "bhadresh-savani/bert-base-go-emotion",
-    #               "bhadresh-savani/roberta-base-emotion",
+    # model_name = ["j-hartmann/emotion-english-roberta-large",
     #               "nateraw/bert-base-uncased-emotion",
+    #               "gokuls/distilroberta-emotion-intent",
+    #               "bergum/xtremedistil-l6-h384-go-emotion",
     #               "gokuls/distilroberta-emotion-intent"]
-    # model_label = ["michellejieli-emotion_text_classifier",
-    #                "bhadresh-savani_bert-base-go-emotion",
-    #                "bhadresh-savani_roberta-base-emotion",
+    # model_label = ['j-hartmann roberta large',
     #                "nateraw_bert-base-uncased-emotion",
-    #                "gokuls_distilroberta-emotion-intent"]
-    
-    model_name = ["bergum/xtremedistil-l6-h384-go-emotion"]
-    model_label = ["bergum_xtremedistil-l6-h384-go-emotion"]
-    
-    # model_name = ["nateraw/bert-base-uncased-emotion",
-    #               "gokuls/distilroberta-emotion-intent"]
-    # model_label = ["nateraw_bert-base-uncased-emotion",
+    #                "gokuls_distilroberta-emotion-intent",
+    #                "bergum_xtremedistil-l6-h384-go-emotion",
     #                "gokuls_distilroberta-emotion-intent"]
     
     if args.mode == 'fine-tuning':
-        train_datas = ['data_fold/data_0/dailydialog_train.json', * 
-                    [f'data_fold/data_{fold_}/data_{fold_}_train.json' for fold_ in range(1, 5)]]
-        test_datas = ['data_fold/data_0/dailydialog_test.json', * 
-                    [f'data_fold/data_{fold_}/data_{fold_}_test.json' for fold_ in range(1, 5)]]
-        data_labels = ['-original_dd', *[f'-data_{fold_}_DailyDialog' for fold_ in range(1, 5)]]
+        # train_datas = ['data_fold/data_0/dailydialog_train.json', * 
+        #             [f'data_fold/data_{fold_}/data_{fold_}_train.json' for fold_ in range(1, 5)]]
+        # test_datas = ['data_fold/data_0/dailydialog_test.json', * 
+        #             [f'data_fold/data_{fold_}/data_{fold_}_test.json' for fold_ in range(1, 5)]]
+        # data_labels = ['-original_dd', *[f'-data_{fold_}_DailyDialog' for fold_ in range(1, 5)]]
+        
+        train_datas = ['data_fold/data_0/dailydialog_train.json']
+        test_datas = ['data_fold/data_0/dailydialog_test.json']
+        data_labels = ['-original_dd']
         
         print("Fine-tuning")
         
@@ -105,6 +103,7 @@ def main():
             args.model_name = mn
             args.model_label = ml
             
+            print("model: ", args.model_name)
             for tr, te, dl in zip(train_datas, test_datas, data_labels):
                 args.train_data, args.test_data, args.data_label = tr, te, dl
                 
@@ -113,17 +112,38 @@ def main():
                 
                 del finetuner
                 
-    elif args.mode == 'test':
-        test_datas = ['data_fold/data_0/dailydialog_test.json', * 
-                    [f'data_fold/data_{fold_}/data_{fold_}_test.json' for fold_ in range(1, 5)]]
-        data_labels = ['-original_dd', *[f'-data_{fold_}_DailyDialog' for fold_ in range(1, 5)]]
-        print("Testing")
+    # elif args.mode == 'test':
+    #     test_datas = ['data_fold/data_0/dailydialog_test.json', * 
+    #                 [f'data_fold/data_{fold_}/data_{fold_}_test.json' for fold_ in range(1, 5)]]
+    #     data_labels = ['-original_dd', *[f'-data_{fold_}_DailyDialog' for fold_ in range(1, 5)]]
+    #     print("Testing fine-tuned")
         
-        for mn, ml in zip(model_name, model_label):
-            tester = Tester(mn, ml, test_datas, data_labels)
-            tester.run()
+    #     for mn, ml in zip(model_name, model_label):
+    #         args.model_name = mn
+    #         args.model_label = ml
+            
+    #         for te, dl in zip(test_datas, data_labels):
+    #             args.test_data, args.data_label = te, dl
+                
+    #             model_file = args.fine_tuned_model
+    #             tester = Finetuner(**vars(args))
+    #             tester.test_b_model(model_file)
+                
+    #             del tester
+    
+    
+    # 이거는 tester.py 를 사용한 테스트 (taxonomy가 같은 모델)
+    # elif args.mode == 'test':
+    #     test_datas = ['data_fold/data_0/dailydialog_test.json', * 
+    #                 [f'data_fold/data_{fold_}/data_{fold_}_test.json' for fold_ in range(1, 5)]]
+    #     data_labels = ['-original_dd', *[f'-data_{fold_}_DailyDialog' for fold_ in range(1, 5)]]
+    #     print("Testing")
+        
+    #     for mn, ml in zip(model_name, model_label):
+    #         tester = Tester(mn, ml, test_datas, data_labels)
+    #         tester.run()
 
-            del tester
+    #         del tester
             
         
 
--- module/finetuner.py
+++ module/finetuner.py
@@ -13,6 +13,7 @@ from module.evaluation import metrics_report, metrics_report_for_emo_binary, Foc
 from module.dataset import get_bulk_texts_and_labels
 import datetime
 import logging
+import copy
 
 emotion_label_policy = {'angry': 0, 'anger': 0,
     'disgust': 1,
@@ -22,8 +23,48 @@ emotion_label_policy = {'angry': 0, 'anger': 0,
     'surprise': 5, 'surprised': 5, 
     'neutral': 6}    
 
+class Full_model(nn.Module):
+    '''
+    Freeze 없이 모든 모델을 학습시키는 형태
+    '''
+    def __init__(self, a_model, b_model):
+        super(Full_model, self).__init__()
+        self.model_1 = a_model
+        self.model_2 = b_model
+        
+        for param in self.model_1.parameters():
+            param.requires_grad = True
+        for param in self.model_2.parameters():
+            param.requires_grad = True
+
+    def forward(self, inputs, masks):
+        input = self.model_1(inputs, masks)[1]
+        output = self.model_2(input)
+        return output
+
+class Model_type3(nn.Module):
+    '''
+    Model_type3은 기존의 pretrained model의 encoder layer를 1개 추가해서 사용하고, 
+    그 뒤에 classifier를 추가하는 형태
+    '''
+    def __init__(self, model, output_size):
+        super(Model_type3, self).__init__()
+        self.layer_1 = copy.deepcopy(model.encoder.layer[0])
+        
+        self.pooler = copy.deepcopy(model.pooler)
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=model.config.hidden_size, out_features=model.config.hidden_size),
+            nn.Dropout(p=0.1, inplace=False),
+            nn.Linear(in_features=model.config.hidden_size, out_features=output_size)   
+        )
+    def forward(self, input):
+        input = self.layer_1(input)[0]
+        input = self.pooler(input)
+        output = self.classifier(input)
+        return output
+        
 class Finetuner:
-    def __init__(self, gpus, model_name, train_data, test_data, data_label, model_label, use_wandb, batch_size, epoch, learning_rate, **kwargs):
+    def __init__(self, gpus, model_name, model_type, train_data, test_data, data_label, model_label, use_wandb, batch_size, epoch, learning_rate, **kwargs):
         # Finetuner가 받아오는 인자들
         self.model_name = model_name
         self.train_data = train_data
@@ -35,6 +76,8 @@ class Finetuner:
         self.epoch = epoch
         self.learning_rate = learning_rate
             
+        self.best_performance_train = 0
+        self.best_performance_test = 0
         # logging 용
         self.start_time = datetime.datetime.now()
         self.data_label = data_label
@@ -42,16 +85,15 @@ class Finetuner:
         self.class_label = np.array(['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral'])
         self.log_directory = kwargs.get('log_directory', None)
         
-        self.logger_train = self.set_logger('train')
-        self.logger_test = self.set_logger('test')
-
+        self.logger_train = self.set_logger(f'train')
+        self.logger_test = self.set_logger(f'test')
         # 모델이 자체적으로 생성
         '''
         a_model = 원래 모델 (사전학습된 감정분류 모델)
         b_model = a_model 위에 추가된 모델 (a_model에 Linear layer를 추가한 모델)
         '''
         
-        self.model_type = 2
+        self.model_type = model_type
             # 1: AutoModelForSequenceClassification에다가 Linear layer (original_taxonomy ➝ n_emotion)
             # 2: AutoModel에다가 바로 Linear layer (hidden_size ➝ n_emotion)
             # 3: Automodel에다가 Transformer layer n개 붙인 뒤 Linear layer (hidden_size ➝ n_emotion)
@@ -59,11 +101,16 @@ class Finetuner:
         self.a_model = self.set_a_model(self.model_name, self.model_type)
         if self.model_type == 1:
             self.original_taxonomy = len(self.a_model.config.label2id) # original model's output size
-            self.b_model = self.set_b_model_as_added_layer(self.a_model, self.original_taxonomy, num_classes=7)
+            self.b_model = self.set_b_model_as_added_layer(self.a_model, input_size=self.original_taxonomy, output_size=7)
         elif self.model_type == 2:
-            self.b_model = self.set_b_model_as_added_layer(self.a_model, self.a_model.config.hidden_size, num_classes=7)
-        
+            self.b_model = self.set_b_model_as_added_layer(self.a_model, input_size=self.a_model.config.hidden_size, output_size=7)
+        elif self.model_type == 3:
+            self.b_model = self.set_b_model_as_transformer_layer(self.a_model, input_size=self.a_model.config.hidden_size, output_size=7)
+        elif self.model_type == 4:
+            self.original_taxonomy = len(self.a_model.config.label2id) # original model's output size
+            self.b_model = self.set_b_model_as_added_layer(self.a_model, input_size=self.original_taxonomy, output_size=7)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.wandb_project_name = f'{self.model_label}_type-{self.model_type}_-train_on_{self.data_label}'
     
     def get_dataset_from_file(self, filename):
         # Load the JSON dataset
@@ -91,58 +138,85 @@ class Finetuner:
     
     def set_a_model(self, model_name, model_type):
         if model_type == 1:
-            model = AutoModelForSequenceClassification.from_pretrained(model_name)
+            model = AutoModelForSequenceClassification.from_pretrained(model_name, from_tf=False)
         elif model_type == 2:
             model = AutoModel.from_pretrained(model_name)
-            
+        elif model_type == 3:
+            model = AutoModel.from_pretrained(model_name)
+        elif model_type == 4:
+            model = AutoModelForSequenceClassification.from_pretrained(model_name, from_tf=False)
+        
+        # self.model_1 = AutoModelForSequenceClassification.from_pretrained(model_name, from_tf=False).cuda()
+        # self.model_2 = AutoModel.from_pretrained(model_name).cuda()
+        # self.model_3 = AutoModel.from_pretrained(model_name).cuda()
+        
         # Freeze the pre-trained model's parameters
-        for param in model.parameters():
-                param.requires_grad = False
+        if (model_type != 4):
+            for param in model.parameters():
+                    param.requires_grad = False
         return model    
     
-    def set_b_model_as_added_layer(self, model, original_taxonomy=7, num_classes=7):
+    def set_b_model_as_added_layer(self, model, input_size=7, output_size=7):
         '''
         Pre-trained Classificaiton 모델 (LM + classification layer) 위에다 또 linear layer를 얹어서
         추가된 linear layer에 분류를 학습
         이 함수는 추가 linear layer(added_model)를 리턴한다
         '''
-        if original_taxonomy == num_classes:
-            # 원래 taxonomy와 같은 경우
-            added_model = nn.Sequential(
-                nn.Linear(in_features=original_taxonomy, out_features=num_classes)
-            )
-        else:
-            added_model = nn.Sequential(
-                nn.Linear(in_features=original_taxonomy, out_features=num_classes),
-                # nn.ReLU(),
-                # nn.Dropout(p=0.1),
-                # nn.Linear(in_features=num_classes, out_features=num_classes)
-            )
+        added_model = nn.Sequential(
+            # nn.Dropout(p=0.5), # 이거 넣어서 다시 해보기 -> 안돼
+            nn.Linear(in_features=input_size, out_features=output_size)
+        )
         return added_model
     
-    def set_b_model_as_transformer_layer(self, model_name, num_classes=7):
-        added_model = nn.Sequential(
-                nn.Linear(in_features=7, out_features=num_classes),
-                # nn.ReLU(),
-                # nn.Dropout(p=0.1),
-                # nn.Linear(in_features=num_classes, out_features=num_classes)
-            )
+    def set_b_model_as_transformer_layer(self, model, input_size=7, output_size=7):
+        # 여기서, layer 개수 설정하는 방법 추가, layer weight 초기화시키기
+        added_model = Model_type3(model, output_size)
+        # Unfreeze the pre-trained model's parameters
+        for param in added_model.parameters():
+                param.requires_grad = True
         return added_model
     
     def model_calculate(self, inputs, masks):
         '''
         model_type에 따라서, a_model -> b_model의 순서로 입력에 대해 각 감정의 score를 계산
+        
+        inputs: [10, 273]
+        masks: [10, 273]
+        
+        output: [10, 273, 1024]
+        pooler output: [10, 1024]
+        
+        전체 b_model의 output은 [10, 7]이 되도록
         '''
+        features={}
+        def get_features(name):
+            def hook(model, input, output):
+                features[name] = output
+            return hook 
+        
         if self.model_type == 1:
-            model_outputs = self.a_model(inputs, masks)[0]  # model_outputs: (batch_size, original_taxonomy)    ex) [6, 28]
-            outputs = self.b_model(model_outputs)           # outputs: (batch_size, num_classes)                ex) [6, 7]
+            model_outputs = self.a_model(inputs, masks)[0]  # model_outputs: (batch_size, original_taxonomy)    ex) [10, 28]
+            outputs = self.b_model(model_outputs)           # outputs: (batch_size, num_classes)                ex) [10, 7]
         elif self.model_type == 2:
+            '''
+            self.a_model(inputs, masks) = (last_hidden_state[10, 273, 1024], pooler_output[10, 1024])
+            model_2.encoder.layer[0].register_forward_hook(get_features('layer0')) # layer0의 output을 features['layer0']에 저장
+            '''
+            # self.model_2.encoder.layer[0].register_forward_hook(get_features('layer0'))
             model_outputs = self.a_model(inputs, masks)[1]  # model_outputs은 pooled_output: (batch_size, hidden_size)
             outputs = self.b_model(model_outputs)           
+        elif self.model_type == 3:
+            model_outputs = self.a_model(inputs, masks)[0]  # [10, 273, 1024]
+            outputs = self.b_model(model_outputs)   # b_model: [10, 273, 1024]->[10, 1024]->[10, 7]            ex) [10, 7]
+        elif self.model_type == 4:
+            outputs = self.a_model(inputs, masks)[0]     
+            
         return outputs
     
     def set_logger(self, logger_name):
-        logger = logging.getLogger(logger_name)
+        logger = logging.getLogger(f'{logger_name}-{self.model_name}-{str(self.start_time).replace(" ", "_")}')
+        if len(logger.handlers) > 0:
+            return logger # Logger already exists
         logger.setLevel(logging.INFO)    
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -166,13 +240,15 @@ class Finetuner:
     def finetune(self):
         # Initialize WandB
         if self.use_wandb:
-            wandb_project_name = f'{self.model_label}_train_on_{self.data_label}'
-            wandb.init(project=wandb_project_name)
+            wandb.init(project=self.wandb_project_name)
         
         self.a_model = self.a_model.cuda()
         self.b_model = self.b_model.cuda()
         
-        optimizer = optim.Adam(self.b_model.parameters(), lr=self.learning_rate)
+        if self.model_type == 4:
+            optimizer = optim.Adam(self.a_model.parameters(), lr=self.learning_rate)
+        else:
+            optimizer = optim.Adam(self.b_model.parameters(), lr=self.learning_rate)
         dataset = self.get_dataset_from_file(self.train_data)
         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
         
@@ -216,7 +292,7 @@ class Finetuner:
             
             self.reporting(emotion_pred, emotion_label, type_label='train')
             # 현재 epoch에 대해서 모델 테스트
-            self.test(epoch)
+            self.test_training(epoch)
             
         # Finish the WandB run
         if self.use_wandb:
@@ -224,7 +300,7 @@ class Finetuner:
             
         self.save_model()
     
-    def test(self, epoch_num):
+    def test_training(self, epoch_num):
         device = 'cuda:0'
         dataset = self.get_dataset_from_file(self.test_data)
         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
@@ -249,8 +325,13 @@ class Finetuner:
         self.logger_test.info('\nEpoch [{}/{}]'.format(epoch_num+1, self.epoch))
         self.reporting(emotion_pred, emotion_label, type_label='test')
         
+    
+    
     def run(self, **kwargs):
         self.finetune()
+        
+        del self.logger_train
+        del self.logger_test
     
     def reporting(self, emotion_pred, emotion_true, type_label='test'):
         '''
@@ -264,14 +345,40 @@ class Finetuner:
         log_label = self.data_label
         start_time = self.start_time
         
-        report = '\n'
-        report += metrics_report(emotion_pred, emotion_true, class_label)
-        report += '\n'+metrics_report_for_emo_binary(emotion_pred, emotion_true)+'\n'
+        report_dict = metrics_report(emotion_pred, emotion_true, class_label, get_dict=True)
+        report = metrics_report(emotion_pred, emotion_true, class_label)
+        result = '\n'+report+'\n'
         
-        if type_label == 'train':
-            self.logger_train.info(report)
+        if type_label == 'test':
+            self.best_performance_test = max(self.best_performance_test, report_dict['macro avg']['f1-score'])
         else:
-            self.logger_test.info(report)    # report를 log에 저장
+            self.best_performance_train = max(self.best_performance_train, report_dict['macro avg']['f1-score'])
+            
+        print(f'folder, file: {self.log_folder}, {self.log_file}')
+        # file_name = f'logs/{self.model_label}_{type_label}_{self.data_label}-{str(self.start_time)}.log'
+        # with open(file_name, 'a') as f:
+        #     for state in self.b_model.state_dict():
+        #         f.write(state+': '+str(self.b_model.state_dict()[state].view(-1)[:5])+'\n')
+        #     f.write('\n')
+            
+        
+        if type_label == 'test':
+            logger = self.logger_test    
+            best_performance = self.best_performance_test
+        else:
+            logger = self.logger_train
+            best_performance = self.best_performance_train
+        
+        if (self.use_wandb and type_label == 'test'): # 감정 별 분류성능 변화를 wandb에 저장
+            for i in self.class_label:
+                wandb.log({f"{i}_f1": report_dict[i]['f1-score']})
+        
+        result += f"Best performance (macro avg): {best_performance}\n"
+        logger.info(result) # report를 log에 저장
+        
+        # # logger 비정상 작동 시 파일로 그냥 직접 저장
+        # with open(file_name, 'a') as f:
+        #     f.write(result+f"Current Performance (macro avg): {report_dict['macro avg']['f1-score']}\nBest performance (macro avg): {best_performance}\n")
     
     def save_model(self):
         torch.save(self.b_model.state_dict(), f'model/{self.model_label}_{self.data_label}.pt')
@@ -296,8 +403,23 @@ class Finetuner:
         )
         added_model = model.classifier
         return added_model
-
-
-
-
-
+    
+    def test_b_model(self):
+        device = 'cuda:0'
+        dataset = self.get_dataset_from_file(self.test_data)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+        
+        with torch.no_grad():
+            emotion_pred, emotion_label = [list() for _ in range(2)]
+            for inputs, masks, labels in tqdm(dataloader, desc=f"Test"):
+                outputs = self.model_calculate(inputs, masks)
+                
+                for pred in outputs:
+                    emotion_pred.append(pred)
+                for label in labels:
+                    emotion_label.append(label)
+        
+        emotion_pred = torch.stack(emotion_pred).cpu()          # 펴서 tensor로 만들어줌
+        emotion_label = torch.stack(emotion_label).cpu()        # 펴서 tensor로 만들어줌
+        self.reporting(emotion_pred, emotion_label, type_label='test')
+        
